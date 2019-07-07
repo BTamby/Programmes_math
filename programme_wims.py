26fd2f28d2f3fffd381a328123e9a6a3b20b2e89
Benoit Tamby <benoit.tamby@ac-versailles.fr>
2019-07-07 13:16:24 +0200

    Initialisation du dépot de création des programmes de math pour le serveur wims

---
+++ programme_wims.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os, sys
+
+def inserer_codejs(prog_edite):
+	texte = '!if $jquery_defined=yes'+'\n<script>\n'+'jQuery(function($$){'+'\n'+'$$(".accordion").accordion({'+\
+	'\n'+'collapsible: true,\n'+'active: false,\n'+'});\n'+'} );\n'+'</script>\n'+'!read tabscript '+ prog_edite+'\n'+\
+'!endif\n'+'!tail'
+	return texte
+
+def info_entete(f_txt,dico):
+	"""Création du dictionnaire d'informations qui constituent l'entête du fichier html du programme créé. """
+	for cle in dico:
+		tag = tag='@'+cle
+		f=open(f_txt,'r')
+		continuer = True
+		while continuer :
+			lign = f.readline()
+			liste = lign.split(':')
+			test_tag = lign.split(':')[0]
+			if test_tag == tag or lign == '':
+				continuer = False
+		f.close()
+		#print('Le tag ',tag,' et la li ',liste)
+		index_2pt = lign.index(':')
+		info_lu = lign[index_2pt+1:-1]
+		#print("info_lu = ",info_lu)
+		dico[cle]=info_lu
+
+def begin_html(dico):
+	"""Début du fichier html avec les informations générales"""
+	texte='<h2 class="wims_title">'+dico['titreniveau']+'</h2>\n<h3 class="wims_title">\n\t<a href='+'"'+dico['lienprogramme']+'" target="wims_external">\n\t\t'+dico['dateprogramme']+'\n\t</a>\n</h3>\n\
+<div class="wims_msg info program_desc">\n\t'+\
+dico['datewims']+'\n\
+</div>\n'
+	return texte
+
+def creer_intro(f_txt):
+	tag ='@intro'
+	texte = ''
+	continuer = True
+	f=open(f_txt,'r')
+	#Avancer au tag @intro
+	while continuer :
+		lign = f.readline()
+		test_tag = lign.split(':')[0]
+		if test_tag == tag :
+			continuer = False
+	continuer = True
+	while continuer:
+		lign = f.readline()
+		#print("Ligne lue =")
+		test_tag_suiv = lign[0]
+		if test_tag_suiv == '@' or lign =='':
+			continuer = False
+		else :
+			texte = texte + lign
+	#print("Intro = \n",texte)
+	f.close()
+	return texte
+
+def ressource_euler(dico):
+	texte = '<div class="wimscenter">\n\t\
+<a class ="external_link" href="'+dico['ressources']+'" target="wims_external" title="Ressources complémentaires sur le site Euler">\
+	\n\t\tRessources complémentaires\n\t</a>\n</div>\n'
+	return texte
+def mise_a_jour_themes(path_fichier,fichier):
+	flux = open(path_fichier)
+	tag_theme = '@theme'
+	tag_fin = '@end'
+	continuer = True
+	liste_themes = []
+	while continuer:
+		lign = flux.readline()
+		tag= lign.split(':')[0]
+		if tag == tag_theme :
+			nom =lign[len(tag)+1:-1]
+			liste_themes.append(nom)
+			#print("Theme : ",nom)
+		if tag == tag_fin :
+			continuer = False
+	flux.close()
+	return liste_themes
+def creer_menu(l_theme):
+	texte = '\n\t<ul class="wims_summary button-group small">\n'
+	for i,e in enumerate(l_theme) :
+		texte = texte +'\t\t<li><a href="#c_'+str(i)+'">'+e+'</a></li>\n'
+	texte = texte + '\t</ul>\n'
+	return texte
+def creer_un_programme(path_fichier,fichier):
+	#Dictionnaire contenant les informations de l'entête
+	elements_info={'titreniveau':'','dateprogramme':'','lienprogramme':'','datewims':'','level':'','ressources':''}
+	program_adress ='/home/wims/public_html/modules/help/teacher/program.fr/fr/'
+	out_phtml =open(program_adress+fichier+'.phtml','w',encoding='Windows 1252')
+	#Mise à jour du dictionnaire d'informations générales pour l'entête
+	info_entete(path_fichier,elements_info)
+	#Entête du fichier html
+	content=begin_html(elements_info)
+	#Création de l'introduction
+	content=content+creer_intro(path_fichier)
+	#Insertion lien vers ressources complémentaires sur le site Euler
+	content=content+ressource_euler(elements_info)
+	#Enregistrement des noms des thèmes dans une liste
+	themes = mise_a_jour_themes(path_fichier,fichier)
+	allmenu = creer_menu(themes)
+	"""allmenu ='\n\t<ul class="wims_summary">\n\
+		<li><a href="#c_0">Algèbre</a></li>\n\
+		<li><a href="#c_4">Organisation et gestion de données, fonctions</a></li>\n\
+		<li><a href="#c_9">Grandeurs et mesures</a></li>\n\
+		<li><a href="#c_12">Espace et géométrie</a></li>\n\
+		<li><a href="#c_15">Algorithmique et programmation</a></li>\n\
+		<li><a href="#c_17">Croisements entre enseignements</a></li>\n\
+	</ul>'"""
+	alltext = '<div id="widget_'+fichier.replace('.','')+'">'+allmenu+'\n</div><!--Fin de widget_'+fichier.replace('.','')+'-->'
+	content=content+alltext
+	code_prog=fichier.replace('.','')
+	content =content+'\n'+inserer_codejs(code_prog)
+	#Ecriture du contenu dans le fichier de sorite .phtml
+	out_phtml.write(content)
+	out_phtml.close()
+
+
+
+work_directory = os.getcwd()
+dossier_matiere = work_directory+'/math/'
+fichier_txt ='math.test'
+path_fichier_txt = dossier_matiere+fichier_txt
+creer_un_programme(path_fichier_txt,fichier_txt)
