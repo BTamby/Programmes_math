c70d7617c86e1ceae55f81385bd4d889336dc041
Benoit Tamby <benoit.tamby@ac-versailles.fr>
2019-07-11 00:33:53 +0200

    Création des colonnes contenus, capacités attendues et commentaires

--- programme_wims_v2.py
+++ programme_wims_v2.py
@@ -62,6 +62,39 @@ dico['datewims']+'\n\
 	f.close()
 	return txt+'\n</div><!-- Fin accordion -->\n</div><!-- Fin du préambule-->\n'
 
+def creer_elements(elt):
+	tag_elt ='@'+elt
+	for them in dico_all :
+		flux = open(path_fichier_txt)
+		les_elts = []
+		tag ='@theme'
+		continuer = True
+		#Avancer au prochain tag @theme
+		while continuer :
+			lign = flux.readline()
+			test_tag = lign.split(':')[0]
+			if test_tag == tag and lign[len(tag)+1:-1] == them:
+				continuer = False
+		continuer = True
+		#theme = lign[len(tag)+1:-1]
+		while continuer :
+			lign = flux.readline()
+			test_tag = lign.split(':')[0]
+			if test_tag == tag or lign =='':
+				continuer = False
+			else :
+				if test_tag == tag_elt:
+					txt = lign[len(tag_elt)+1:-1]
+					lign = flux.readline()
+					while lign[0] != '@':
+						txt=txt+lign
+						lign = flux.readline()
+
+					les_elts.append(txt)
+		dico_all[them][elt]=les_elts
+		#print("Les éléments :",les_elts)
+		flux.close()
+	
 def creer_les_titres():
 
 	for them in dico_all :
@@ -88,37 +121,6 @@ def creer_les_titres():
 		dico_all[them]['titres']=les_titres
 		#print("Les titres :",les_titres)
 		flux.close()
-def creer_les_contenus():
-	for them in dico_all :
-		flux = open(path_fichier_txt)
-		les_contenus = []
-		tag ='@theme'
-		continuer = True
-		#Avancer au prochain tag @theme
-		while continuer :
-			lign = flux.readline()
-			test_tag = lign.split(':')[0]
-			if test_tag == tag and lign[len(tag)+1:-1] == them:
-				continuer = False
-		continuer = True
-		#theme = lign[len(tag)+1:-1]
-		while continuer :
-			lign = flux.readline()
-			test_tag = lign.split(':')[0]
-			if test_tag == tag or lign =='':
-				continuer = False
-			else :
-				if test_tag == '@contenu':
-					txt = lign[len('@contenu')+1:-1]
-					lign = flux.readline()
-					while lign[0] != '@':
-						txt=txt+lign
-						lign = flux.readline()
-
-					les_contenus.append(txt)
-		dico_all[them]['contenu']=les_contenus
-		#print("Les titres :",les_titres)
-		flux.close()
 
 def creer_les_themes():
 	flux = open(path_fichier_txt)
@@ -133,7 +135,6 @@ def creer_les_themes():
 			nom =lign[len(tag)+1:-1]
 			themes.append(nom)
 			dico_all[nom]={}
-			#Ajouter objectif et histoire dans le dictionnaire
 			creer_presentation(flux,nom)
 		if tag == tag_fin :
 			continuer = False
@@ -155,7 +156,6 @@ def creer_presentation(flux,them):
 	while lign.split(':')[0] != end_realine :
 		txt_histoire = txt_histoire +lign
 		lign =flux.readline()
-	#print("Dernière ligne de présentation : ",lign)
 	dico_all[them]['histoire']=txt_histoire
 
 
@@ -164,19 +164,19 @@ work_directory = os.getcwd()
 dossier_matiere = work_directory+'/math/'
 fichier_txt ='math.1G_test'
 path_fichier_txt = dossier_matiere+fichier_txt
-allhtml = ''
+#allhtml = ''
 program_adress ='/home/wims/public_html/modules/help/teacher/program.fr/fr/'
 out_phtml =open(program_adress+fichier_txt+'.phtml','w',encoding='Windows 1252')
-txt_html = begin_html()
-allhtml = allhtml + txt_html
+#txt_html = begin_html()
+#allhtml = allhtml + txt_html
+allhtml = begin_html()
 dico_all = {}
 #Creer le contenu de chaque thème
 creer_les_themes()
 creer_les_titres()
-creer_les_contenus()
-#print("Les thèmes : ", les_themes)
-for them in dico_all :
-	print(them,' : ',dico_all[them])
+creer_elements('contenu')
+creer_elements('capacite')
+creer_elements('commentaire')
 #Insérer le menu dans  le fichier .phtml
 allhtml=allhtml+'<div id="widget_'+fichier_txt.replace('.','')+'"><!-- Début de id = widget_'+fichier_txt.replace('.','')+'-->'
 allhtml = allhtml+'\n\t<ul class="wims_summary"><!-- Début du menu -->\n'
@@ -196,16 +196,14 @@ for cle,val in dico_all.items():
 	begin_div_objectif = '\t\t<div class="accordion">\n\t\t\t<h4>Objectifs</h4>\n\t\t\t<div>\n'
 	allhtml = allhtml+begin_div_objectif
 	allhtml = allhtml+val['objectif']
-	end_div_objectif = '\t\t\t</div>\n\t\t</div><!--Fin objectifs-->\n'
+	end_div_objectif = '\n\t\t\t</div><!--Fin objectifs-->\n\t\t</div><!--Fin accordion-->\n'
 	allhtml = allhtml + end_div_objectif
 
 	begin_div_histoire = '\t\t<div class="accordion">\n\t\t\t<h4>Histoire des mathématiques</h4>\n\t\t\t<div>\n'
 	allhtml = allhtml+begin_div_histoire
 	allhtml = allhtml+val['histoire']
-	end_div_histoire = '\t\t\t</div>\n\t\t</div><!--Fin histoire des maths-->\n'
+	end_div_histoire = '\n\t\t\t</div><!--Fin histoire des maths-->\n\t\t</div><!--Fin accordion-->\n'
 	allhtml = allhtml + end_div_histoire
-	
-	
 	allhtml = allhtml +'\t<h4>Sommaire du thème '+cle+'</h4><!-- Sommaire-->\n'
 	allhtml = allhtml+'<ul class="program_submenu">\n\t'
 	for num,t in enumerate(dico_all[cle]['titres']):
@@ -213,18 +211,30 @@ for cle,val in dico_all.items():
 	allhtml = allhtml+'\n</ul>\n'
 	#Contenus, capacités, commentaires
 	for num,val in enumerate(dico_all[cle]['contenu']):
-		begin_div_bloc = '<div id = "t_'+str(i)+str(num)+'">\n'
-		allhtml = allhtml +begin_div_bloc+'<h4 class="program_titre">'+dico_all[cle]['titres'][num]+'</h4>\n'
-		allhtml = allhtml +'<div class = "small-4">\n'
+		allhtml = allhtml+'<h4 class="program_titre">'+dico_all[cle]['titres'][num]+'</h4>\n'
+		begin_div_bloc = '<div id = "t_'+str(i)+str(num)+'" class="grid-x grid-margin-x medium-margin-collapse"><!-- Début bloc -->\n'
+		allhtml = allhtml +begin_div_bloc
+		#Colonne des contenus
+		allhtml = allhtml +'<div class="box_content2 small-4 medium-4 large-4 cell "><!--Colonne contenu-->\n<h4 class="c">Contenus</h4>\n'
 		allhtml = allhtml +val
-		allhtml = allhtml + '</div><!-- Fin class small-->\n'
-		end_div_bloc ='</div><!-- Fin du bloc -->\n'
+		allhtml = allhtml + '</div><!-- Fin colonne contenu-->\n'
+		#colonne des cpacités
+		allhtml = allhtml +'<div class="box_content2 small-4 medium-4 large-4 cell "><!--Colonne capacités-->\n<h4 class="c">Capacités atendues</h4>\n'
+		allhtml = allhtml +dico_all[cle]['capacite'][num]
+		allhtml = allhtml + '</div><!-- Fin colonne capacites-->\n'
+		#Colonnes compléments
+		allhtml = allhtml +'<div class="box_content2 small-4 medium-4 large-4 cell "><!--Colonne commentaires-->\n<h4 class="c">Activités à réaliser</h4>\n'
+		allhtml = allhtml +dico_all[cle]['commentaire'][num]
+		allhtml = allhtml + '\n</div><!-- Fin colonne commentaires-->\n'
+		allhtml = allhtml +'<div class="box_content2 small-12 cell"><!-- Les exercices -->\nInsérer ici les\
+exercices du paragraphe '+ dico_all[cle]['titres'][num]+'<br/></div><!-- Fin exercices -->\n'
+		end_div_bloc ='</div><!-- Fin bloc -->\n'
+		
 		allhtml = allhtml + end_div_bloc
 	#Fin du thème
 	end_div='\t</div><!--End thème '+cle+'-->\n'
 	allhtml = allhtml + end_div
 	i += 1
-#Créer le sommaire
 
 # Fin du fichier phtml
 allhtml = allhtml+'</div><!-- id = widget_'+fichier_txt.replace('.','')+'-->'
@@ -235,6 +245,4 @@ allhtml = allhtml+'\n'+inserer_codejs(nom_du_prog)
 out_phtml.write(allhtml)
 out_phtml.close()
 
-# Ouvrir le fichier de sortie phtml en écriture
-#creer_un_programme(fichier_txt)
 
